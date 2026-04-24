838d4dcb59c2af7a7e0fb41c88f18724a9698421
稲葉 悠成 <inaba-yu2604@ibjapan.jp>
2026-04-24 15:16:26 +0900

    chapter01

---
+++ chapter 01/script.js
@@ -0,0 +1,5 @@
+document.querySelector('p').textContent = 'Hello World!!';
+//window.alert('Hello World');
+console.log('Hello World!!')
+document.write('Hello World!!');
+document.querySelector('p').textContent = 'Hello World!!';
